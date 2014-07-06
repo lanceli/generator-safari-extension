e28fd8135e93f649ea7e97623ef830353dc166a2
Lance Li <lyfuture@gmail.com>
2014-07-06 11:01:12 +0800

    add test

--- test/test-creation.js
+++ test/test-creation.js
@@ -25,7 +25,59 @@ describe('safari-extension generator', function () {
     ];
 
     helpers.mockPrompt(this.app, {
-      'someOption': true
+      'CFBundleDisplayName': 'test',
+      'Author': 'tester',
+      'Description': 'Description',
+      'CFBundleIdentifier': 'com.yourcompany.test',
+      'CFBundleShortVersionString': '0.1.0',
+      'CFBundleVersion': '0.1.0',
+      features: []
+    });
+    this.app.options['skip-install'] = true;
+    this.app.run({}, function () {
+      helpers.assertFile(expected);
+      done();
+    });
+  });
+
+  it('creates expected files with gloabl page', function (done) {
+    var expected = [
+      // add files you expect to exist here.
+      '.jshintrc',
+      '.editorconfig'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'CFBundleDisplayName': 'test',
+      'Author': 'tester',
+      'Description': 'Description',
+      'CFBundleIdentifier': 'com.yourcompany.test',
+      'CFBundleShortVersionString': '0.1.0',
+      'CFBundleVersion': '0.1.0',
+      features: ['globalpage']
+    });
+    this.app.options['skip-install'] = true;
+    this.app.run({}, function () {
+      helpers.assertFile(expected);
+      done();
+    });
+  });
+
+  it('creates expected files with options page', function (done) {
+    var expected = [
+      // add files you expect to exist here.
+      '.jshintrc',
+      '.editorconfig'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'CFBundleDisplayName': 'test',
+      'Author': 'tester',
+      'Description': 'Description',
+      'CFBundleIdentifier': 'com.yourcompany.test',
+      'CFBundleShortVersionString': '0.1.0',
+      'CFBundleVersion': '0.1.0',
+      features: ['globalpage', 'optionspage']
     });
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
